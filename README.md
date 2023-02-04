983ee1c3c14d52177b1a8d328350a6f0b7436b63
Dara Curran <dara.curran@orpheus-cyber.com>
2023-02-04 16:27:00 +0000

    added solution for odd occurences in array and updated README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # my_codility_solutions
 My solutions to the Codility lessons at https://app.codility.com/programmers/lessons.
+
+## Solutions done out for the following questions:
+1. https://app.codility.com/programmers/lessons/3-time_complexity/perm_missing_elem/
+2. https://app.codility.com/programmers/lessons/2-arrays/odd_occurrences_in_array/
--- my_solutions.py
+++ my_solutions.py
@@ -8,6 +8,22 @@ def perm_missing_elem(A):
     else:
         return 1
 
+def odd_occurences_array(A):
+    element_freq = dict()
+    for elem in A:
+        if elem in element_freq.keys():
+            freq = element_freq.get(elem) + 1
+            element_freq.update({elem:freq})
+        else:
+            element_freq.update({elem:1})
+
+    odd_occurences = []
+    for key, value in element_freq.items():
+        if value % 2 != 0:
+            odd_occurences.append(key)
+    return odd_occurences
+
 
 if __name__ == '__main__':
     print(perm_missing_elem([1,2,3,4,6]))
+    print(odd_occurences_array([1,1,1,2,2,3,3,3,3,4]))
