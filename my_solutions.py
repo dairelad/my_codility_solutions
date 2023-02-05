d2e7a5582b668b0f733978dc592d1a8a7c0972e3
Dara Curran <dara.curran@orpheus-cyber.com>
2023-02-05 15:21:14 +0000

    completed missing integer question

--- my_solutions.py
+++ my_solutions.py
@@ -29,7 +29,7 @@ def odd_occurences_array(A):
             odd_occurences.append(key)
     return odd_occurences
 
-def tape_equilibrium(A):
+def tape_equilibrium(A): #todo: revisit for max marks
     '''
     returns the minimum sum of adjacent
     numbers in an array
@@ -44,7 +44,7 @@ def tape_equilibrium(A):
         results.append(abs(lhs-rhs))
     return min(results)
 
-def perm_check(A): #todo: submit on codility for score
+def perm_check(A):
     '''
     returns 1 if the input array is a permutation.
     (sequence of unique integers from 1-N) Else 0
@@ -56,13 +56,13 @@ def perm_check(A): #todo: submit on codility for score
                 return 0
         return 1
 
-def max_counter(A, N): #todo: submit on codility for score
+def max_counter(A, N): #todo: revisit for max marks
     '''
     count occurences in an array according to the max
     counter method, N being the max counter
     '''
     counts = {}
-    for i in range(len(A)-1):
+    for i in range(N):
         counts.update({i+1:0})
 
     for i in range(len(A)):
@@ -72,13 +72,25 @@ def max_counter(A, N): #todo: submit on codility for score
         elif A[i] > N:
             max_c = max(list(counts.values()))
             counts = {x:max_c for x in counts}
-    return counts
 
-def missing_int(A):
+    result =[]
+    for key, value in counts.items():
+        result.append(value)
+    return result
+
+def missing_int(A): #todo: revisit for max marks
     '''
     returns the smallest positive integer that does not occur
     in a given sequence.
     '''
+    a_sorted = sorted(set(A))
+    if a_sorted[-1] <= 0:
+        return 1
+    else:
+        for i in range(len(a_sorted)):
+            if a_sorted[i] != i+1:
+                return i+1
+        return A[-1] + 1
 
 if __name__ == '__main__':
     print('perm missing element:')
@@ -97,4 +109,4 @@ if __name__ == '__main__':
     print(max_counter([3,4,4,6,1,4,4],5))
 
     print('\nmissing int:')
-    print(missing_int())
+    print(missing_int([1,3,6,4,1,2]))
